746445bf69071c399d750d8f7be64320f5b478a9
maderaja <maderajaadi@gmail.com>
2020-11-15 21:34:53 +0700

    Add functionality to edit profile using multipart file to MongoDB Database

--- domain/user.go
+++ domain/user.go
@@ -2,13 +2,15 @@ package domain
 
 import (
 	"MongoDB-CRUD-Operation/response"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct{
-	UserId string `json:"user_id"`
+	UserId primitive.ObjectID `json:"user_id"`
 	FirstName string `json:"first_name"`
 	LastName string `json:"last_name"`
 	Address string  `json:"address"`
+	ProfilePicture string `json:"profile_picture"`
 }
 
 func (user *User) ValidateUser() *response.RestBody{
